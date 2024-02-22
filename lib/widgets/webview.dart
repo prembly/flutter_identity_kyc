91f63a1a0fb1062b4e8311c7ce0789dc9052fbb7
kayodeprembly <123843127+kayodeprembly@users.noreply.github.com>
2024-02-22 21:52:51 +0100

    Update webview.dart

--- lib/widgets/webview.dart
+++ lib/widgets/webview.dart
@@ -6,115 +6,134 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class IdentityKYCWebView extends StatelessWidget {
   final String merchantKey;
-
   final String email;
-
   final String? firstName;
-
   final String? lastName;
-
   final String? userRef;
-
   final String config;
-
   final Function onCancel;
-
   final Function onVerified;
-
   final Function onError;
 
-  IdentityKYCWebView(
-      {required this.merchantKey,
-      required this.email,
-      required this.config,
-      this.firstName,
-      this.lastName,
-      this.userRef,
-      required this.onCancel,
-      required this.onVerified,
-      required this.onError});
+  IdentityKYCWebView({
+    required this.merchantKey,
+    required this.email,
+    required this.config,
+    this.firstName,
+    this.lastName,
+    this.userRef,
+    required this.onCancel,
+    required this.onVerified,
+    required this.onError,
+  });
 
   @override
   Widget build(BuildContext context) {
     InAppWebViewController _webViewController;
+    final GlobalKey<NavigatorState> navigatorKey =
+        GlobalKey<NavigatorState>();
 
     return new WillPopScope(
       onWillPop: () async => false,
       child: Material(
         type: MaterialType.transparency,
         child: SafeArea(
-          child: InAppWebView(
-            initialUrlRequest: URLRequest(
-                url: Uri.parse("https://mobile.prembly.com?merchantKey=" +
-                    merchantKey +
-                    "&firstName=" +
-                    firstName! +
-                    "&lastName=" +
-                    lastName! +
-                    "&email=" +
-                    email +
-                    "&config_id=" +
-                    config +
-                    "&user_ref=" +
-                    userRef! +
-                    "&isTest=false")),
-            initialOptions: InAppWebViewGroupOptions(
-                crossPlatform: InAppWebViewOptions(
-                    mediaPlaybackRequiresUserGesture: false),
-                ios: IOSInAppWebViewOptions(allowsInlineMediaPlayback: true)),
-            gestureRecognizers: {}..add(Factory<LongPressGestureRecognizer>(
-                () => LongPressGestureRecognizer())),
-            onWebViewCreated: (InAppWebViewController controller) {
-              _webViewController = controller;
-
-              _webViewController.addJavaScriptHandler(
-                handlerName: 'message',
-                callback: (args) {
-                  try {
-                    Map response = json.decode(args[0]);
-                    if (response.containsKey("event")) {
-                      switch (response["event"]) {
-                        case "closed":
-                          onCancel({"status": "closed"});
-                          break;
-                        case "error":
-                          onError({
-                            "status": "error",
-                            "message": response['message']
-                          });
-                          break;
-                        case "verified":
-                          onVerified({
-                            "status": "success",
-                            "data": response,
-                          });
-                          break;
-                        default:
-                          break;
-                      }
-                    }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
+          child: Navigator(
+            key: navigatorKey,
+            onGenerateRoute: (settings) {
+              return MaterialPageRoute(
+                builder: (context) => InAppWebView(
+                  initialUrlRequest: URLRequest(
+                    url: Uri.parse(
+                      "https://mobile.prembly.com?merchantKey=" +
+                          merchantKey +
+                          "&firstName=" +
+                          firstName! +
+                          "&lastName=" +
+                          lastName! +
+                          "&email=" +
+                          email +
+                          "&config_id=" +
+                          config +
+                          "&user_ref=" +
+                          userRef! +
+                          "&isTest=false",
+                    ),
+                  ),
+                  initialOptions: InAppWebViewGroupOptions(
+                    crossPlatform: InAppWebViewOptions(
+                      mediaPlaybackRequiresUserGesture: false,
+                    ),
+                    ios: IOSInAppWebViewOptions(
+                      allowsInlineMediaPlayback: true,
+                    ),
+                  ),
+                  gestureRecognizers: {}..add(
+                    Factory<LongPressGestureRecognizer>(
+                      () => LongPressGestureRecognizer(),
+                    ),
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    _webViewController = controller;
+                    _webViewController.addJavaScriptHandler(
+                      handlerName: 'message',
+                      callback: (args) {
+                        try {
+                          Map response = json.decode(args[0]);
+                          if (response.containsKey("event")) {
+                            switch (response["event"]) {
+                              case "closed":
+                                onCancel({"status": "closed"});
+                                break;
+                              case "error":
+                                onError({
+                                  "status": "error",
+                                  "message": response['message'],
+                                });
+                                break;
+                              case "verified":
+                                onVerified({
+                                  "status": "success",
+                                  "data": response,
+                                });
+                                break;
+                              default:
+                                break;
+                            }
+                          }
+                        } catch (e) {
+                          print(e);
+                        }
+                      },
+                    );
+                  },
+                  androidOnPermissionRequest: (
+                    InAppWebViewController controller,
+                    String origin,
+                    List<String> resources,
+                  ) async {
+                    return PermissionRequestResponse(
+                      resources: resources,
+                      action: PermissionRequestResponseAction.GRANT,
+                    );
+                  },
+                  onLoadStop: (controller, url) async {
+                    await controller.evaluateJavascript(
+                      source: """
+                        window.addEventListener("message", (event) => {
+                          window.flutter_inappwebview
+                              .callHandler('message',event.data);
+                        }, false);
+                      """,
+                    );
+                  },
+                  onConsoleMessage: (
+                    InAppWebViewController controller,
+                    ConsoleMessage consoleMessage,
+                  ) {},
+                ),
               );
             },
-            androidOnPermissionRequest: (InAppWebViewController controller,
-                String origin, List<String> resources) async {
-              return PermissionRequestResponse(
-                  resources: resources,
-                  action: PermissionRequestResponseAction.GRANT);
-            },
-            onLoadStop: (controller, url) async {
-              await controller.evaluateJavascript(source: """
-              window.addEventListener("message", (event) => {
-                window.flutter_inappwebview
-                      .callHandler('message',event.data);
-              }, false);
-            """);
-            },
-            onConsoleMessage: (InAppWebViewController controller,
-                ConsoleMessage consoleMessage) {},
           ),
         ),
       ),
